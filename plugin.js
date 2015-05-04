bd1df6b1339c1abe2c87353cdd5bb4f887b559c5
maps4all <maps4all007@yandex.com>
2015-05-04 12:59:59 +0200

    Initial version.

---
+++ plugin.js
@@ -0,0 +1,53 @@
+{
+    "moduleId" : "maps4all/Roadworks",
+    "title" : "Roadworks",
+    "subtitle" : "www.roadworks.com",
+    
+    "backButton" : true,
+    "enableGPS" : false,
+    "zoomControl" : false,
+    "screenLockRot" : false,
+    "reload" : true,
+    
+    "shouldOverrideUrlLoading1" : 'http',
+    "shouldOverrideUrlLoading2" : 'https',
+
+    "loadUrl" : null,
+
+    "loadDataWithBaseUrl1" : 'http://roadworks.org',
+    
+    "loadDataWithBaseUrl3" : "text/html",
+    "loadDataWithBaseUrl4" : "utf-8",
+    "loadDataWithBaseUrl5" : null,
+    "browserLaunchLink" : "http://roadworks.org",
+    
+    "loadDataWithBaseUrl2" : "
+<script type='text/javascript'>
+    
+    // Set date
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    } 
+
+    if(mm<10) {
+        mm='0'+mm
+    } 
+
+    today =     yyyy+'-'+mm+'-'+dd;
+    
+    // Set center and zoom
+    var center = {lat : #lat#, lng : #lng#, zoom : #zoom#};
+    localStorage.setItem('position/', JSON.stringify(center));
+    localStorage.setItem('cookiesAcknowledged', 'true');
+    localStorage.setItem('cookiesAcknowledgedDate', today);
+    window.location.href = 'http://www.roadworks.org';
+    
+</script>      
+    
+    "   
+}
